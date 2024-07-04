8c885955fe3c81c797f866f3c2e5202f27d316d5
Abbas Muhammad <abbas.usafzai@gmail.com>
2024-07-04 21:40:50 +0400

    upgrade angular and cil to 18.0.6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SocketService } from './socket.service';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [SocketService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule], providers: [SocketService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
